75974196af57cdd61dca0b736221d741ce44d581
PC-Ngumoha <pcngumoha@gmail.com>
2025-08-21 08:26:49 +0100

    Display horizon line on screen

--- main.py
+++ main.py
@@ -2,12 +2,34 @@
 main.py: Gino game.
 """
 import pygame
+import os
 
 pygame.init()
+
+# Constants
+
+
+# Colors
+WHITE = (255, 255, 255)
+
+# Dimensions
 SCREEN_WIDTH, SCREEN_HEIGHT = 720, 400
+
+# Setup
 WINDOW = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Gino")
+
+# Images
+HORIZON = pygame.image.load(os.path.join('Assets', 'sprites', 'Horizon.png'))
+
+
+def draw_window():
+    """Handles repainting of screen surface
+    """
+    WINDOW.fill(WHITE)
+    WINDOW.blit(HORIZON, (0, SCREEN_HEIGHT//2))
+    pygame.display.update()
 
 
 def main():
@@ -20,6 +42,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_running = False
+
+        draw_window()
 
     pygame.quit()
 
