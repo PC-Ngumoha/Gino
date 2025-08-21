944d45eb3be321e31fda003419e6af6baf076d1c
PC-Ngumoha <pcngumoha@gmail.com>
2025-08-21 08:09:16 +0100

    Milestone: Gathered game asset

--- main.py
+++ main.py
@@ -2,3 +2,27 @@
 main.py: Gino game.
 """
 import pygame
+
+pygame.init()
+SCREEN_WIDTH, SCREEN_HEIGHT = 720, 400
+WINDOW = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption("Gino")
+
+
+def main():
+    """main code for the game.
+    """
+    game_running = True
+
+    while game_running:
+        # Poll for events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_running = False
+
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
