609bd06eb604a697dccc056cd44cffbc5388921b
PC-Ngumoha <pcngumoha@gmail.com>
2025-08-26 15:00:39 +0100

    Revert "Milestone: Refactor Dino into seperate class."
    This reverts commit 43e637fe064da76f417820496f9ed88c84c0147f.

--- main.py
+++ main.py
@@ -101,6 +101,8 @@ def draw_window(dino, clouds, obstacles, play=True):
             horizon_scroll_x = 0
 
     else:
+        # Dino stands
+        # WINDOW.blit(DINO_STANDING, (dino.x, dino.y))
         dino.stand()
 
     dino.update(WINDOW)
