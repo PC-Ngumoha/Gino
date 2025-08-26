df28bd460e1431b79be395ae1d06fe2d69c7d18f
PC-Ngumoha <pcngumoha@gmail.com>
2025-08-26 15:00:50 +0100

    Revert "Milestone: Parallax effect on horizon"
    This reverts commit c90e93f8ebe57611c672f28cd5a7e4a96e6fd3f6.

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ main.py: Gino game.
 """
 import pygame
 import os
-import random
 
 pygame.init()
 
 # Constants
-FPS = 60
+
 
 # Colors
 WHITE = (255, 255, 255)
@@ -22,46 +21,29 @@ WINDOW = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Gino")
 
 # Images
-HORIZON = pygame.transform.scale(pygame.image.load(os.path.join(
-    'Assets', 'sprites', 'Horizon.png')), (SCREEN_WIDTH - 10, 10)).convert_alpha()
-# CLOUD = pygame.image.load(os.path.join(
-#     'Assets', 'sprites', 'Cloud.png')).convert_alpha()
-
-horizon_width = HORIZON.get_width()
-
-tiles = 2
-scroll_x = 0
+HORIZON = pygame.image.load(os.path.join('Assets', 'sprites', 'Horizon.png'))
 
 
 def draw_window():
     """Handles repainting of screen surface
     """
-    global scroll_x
-
     WINDOW.fill(WHITE)
-    for i in range(0, tiles):
-        WINDOW.blit(HORIZON, (i * horizon_width + scroll_x, SCREEN_HEIGHT//2))
-
-    scroll_x -= 1
-
-    if abs(scroll_x) > horizon_width:
-        scroll_x = 0
+    WINDOW.blit(HORIZON, (0, SCREEN_HEIGHT//2))
+    pygame.display.update()
 
 
 def main():
     """main code for the game.
     """
     game_running = True
-    clock = pygame.time.Clock()
 
     while game_running:
-        clock.tick(FPS)
         # Poll for events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_running = False
+
         draw_window()
-        pygame.display.update()
 
     pygame.quit()
 
