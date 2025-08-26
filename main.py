04b6e588043c81536f823e2b5e56777af6bf1bc2
PC-Ngumoha <pcngumoha@gmail.com>
2025-08-26 15:00:50 +0100

    Revert "Milestone: Attempt cloud parallax"
    This reverts commit a610eaae1ab8960ec55b96368ae1cd9747878227.

--- main.py
+++ main.py
@@ -23,43 +23,29 @@ pygame.display.set_caption("Gino")
 
 # Images
 HORIZON = pygame.transform.scale(pygame.image.load(os.path.join(
-    'Assets', 'sprites', 'Horizon.png')), (SCREEN_WIDTH - 0, 10)).convert_alpha()
-CLOUD = pygame.transform.scale(pygame.image.load(os.path.join(
-    'Assets', 'sprites', 'Cloud.png')), (50, 25)).convert_alpha()
+    'Assets', 'sprites', 'Horizon.png')), (SCREEN_WIDTH - 10, 10)).convert_alpha()
+# CLOUD = pygame.image.load(os.path.join(
+#     'Assets', 'sprites', 'Cloud.png')).convert_alpha()
 
 horizon_width = HORIZON.get_width()
-cloud_width = CLOUD.get_width()
+
+tiles = 2
 scroll_x = 0
-num_clouds = random.randint(1, 2)
 
 
 def draw_window():
     """Handles repainting of screen surface
     """
     global scroll_x
-    global num_clouds
-
-    horizon_tiles = 2
-
-    horizon_x = (scroll_x * 0.8)
-    cloud_x = (scroll_x * 0.4)
 
     WINDOW.fill(WHITE)
-
-    WINDOW.blit(CLOUD, (100 + cloud_x, 20))
-
-    # Render parallax horizon
-    for i in range(0, horizon_tiles):
-        WINDOW.blit(HORIZON, (i * horizon_width + horizon_x, SCREEN_HEIGHT//2))
+    for i in range(0, tiles):
+        WINDOW.blit(HORIZON, (i * horizon_width + scroll_x, SCREEN_HEIGHT//2))
 
     scroll_x -= 1
 
     if abs(scroll_x) > horizon_width:
         scroll_x = 0
-        # num_clouds = random.randint(1, 2)
-
-    # if abs(cloud_x) > cloud_width:
-    #     cloud_x %= cloud_width
 
 
 def main():
