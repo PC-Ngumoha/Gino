9d75855bce0542c402b2287bc42bf09bf696c05a
PC-Ngumoha <pcngumoha@gmail.com>
2025-08-26 15:00:49 +0100

    Revert "Milestone: Implement parallax clouds"
    This reverts commit 2f64fbe61139d6c34d5e15621b80824e00408af6.

--- main.py
+++ main.py
@@ -4,13 +4,11 @@ main.py: Gino game.
 import pygame
 import os
 import random
-from math import fabs
 
 pygame.init()
 
 # Constants
 FPS = 60
-CLOUD_WIDTH, CLOUD_HEIGHT = 50, 25
 
 # Colors
 WHITE = (255, 255, 255)
@@ -27,41 +25,41 @@ pygame.display.set_caption("Gino")
 HORIZON = pygame.transform.scale(pygame.image.load(os.path.join(
     'Assets', 'sprites', 'Horizon.png')), (SCREEN_WIDTH - 0, 10)).convert_alpha()
 CLOUD = pygame.transform.scale(pygame.image.load(os.path.join(
-    'Assets', 'sprites', 'Cloud.png')), (CLOUD_WIDTH, CLOUD_HEIGHT)).convert_alpha()
-
-# Events
-GENERATE_MORE_CLOUDS = pygame.USEREVENT + 1
+    'Assets', 'sprites', 'Cloud.png')), (50, 25)).convert_alpha()
 
 horizon_width = HORIZON.get_width()
-horizon_scroll_x = 0
-cloud_scroll_x = 0
+cloud_width = CLOUD.get_width()
+scroll_x = 0
+num_clouds = random.randint(1, 2)
 
 
-def draw_window(clouds):
+def draw_window():
     """Handles repainting of screen surface
     """
-    global horizon_scroll_x
-    global cloud_scroll_x
+    global scroll_x
+    global num_clouds
 
-    horizon_tiles = 3
+    horizon_tiles = 2
+
+    horizon_x = (scroll_x * 0.8)
+    cloud_x = (scroll_x * 0.4)
 
     WINDOW.fill(WHITE)
 
-    # Render parallax clouds
-    for cloud in clouds:
-        WINDOW.blit(CLOUD, (cloud.x + cloud_scroll_x, cloud.y))
+    WINDOW.blit(CLOUD, (100 + cloud_x, 20))
 
     # Render parallax horizon
     for i in range(0, horizon_tiles):
-        WINDOW.blit(HORIZON, (i * horizon_width +
-                    horizon_scroll_x, SCREEN_HEIGHT//2))
+        WINDOW.blit(HORIZON, (i * horizon_width + horizon_x, SCREEN_HEIGHT//2))
 
-    horizon_scroll_x -= 0.8
-    cloud_scroll_x -= 0.4
+    scroll_x -= 1
 
-    if fabs(horizon_scroll_x) > horizon_width:
-        pygame.event.post(pygame.event.Event(GENERATE_MORE_CLOUDS))
-        horizon_scroll_x = 0
+    if abs(scroll_x) > horizon_width:
+        scroll_x = 0
+        # num_clouds = random.randint(1, 2)
+
+    # if abs(cloud_x) > cloud_width:
+    #     cloud_x %= cloud_width
 
 
 def main():
@@ -70,30 +68,13 @@ def main():
     game_running = True
     clock = pygame.time.Clock()
 
-    # Generate a random number of clouds to move in parallax
-    clouds = [pygame.Rect((i+1) * 200, (i+1) * 15, CLOUD_WIDTH, CLOUD_HEIGHT)
-              for i in range(random.randint(1, 3))]
-
     while game_running:
         clock.tick(FPS)
         # Poll for events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_running = False
-
-            # Catching and handling generate cloud events
-            if event.type == GENERATE_MORE_CLOUDS:
-                # New starting points
-                cloud_x = clouds[-1].x + random.randint(100, 300)
-                cloud_y = random.randint(15, 45)
-
-                clouds = clouds[1:]  # Shift out the first cloud
-
-                # Add a new set of clouds at the tailend of the initial set
-                clouds.extend([pygame.Rect(cloud_x + i*200, cloud_y,
-                              CLOUD_WIDTH, CLOUD_HEIGHT) for i in range(random.randint(1, 3))])
-
-        draw_window(clouds)
+        draw_window()
         pygame.display.update()
 
     pygame.quit()
