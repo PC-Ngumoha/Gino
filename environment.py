cd02bf4e339b2796e8081e55c1d34de0e257fa7b
PC-Ngumoha <pcngumoha@gmail.com>
2025-08-26 14:59:57 +0100

    Revert "Milestone: Refactor obstacles into Environment class"
    This reverts commit 6592051e43709034ccc518a5d7d2d4732f2a4b2d.

--- environment.py
+++ environment.py
@@ -11,43 +11,32 @@ HORIZON = pygame.image.load(os.path.join(
     'Assets', 'sprites', 'Horizon.png'))
 CLOUD = pygame.image.load(os.path.join(
     'Assets', 'sprites', 'Cloud.png'))
-ONE_CACTUS = pygame.image.load(os.path.join(
-    'Assets', 'sprites', '1_Cactus.png'))
 
 
 class Environment:
     """Wrapper class for handling the environment."""
 
-    def __init__(self, screen_width, screen_height, c_width, c_height, o_width, o_height):
+    def __init__(self, screen_width, screen_height, c_width, c_height):
         self.horizon_tiles = 3
         self.horizon_height = 10
         self.horizon_velocity = 1.2
         self.cloud_velocity = 0.5
         self.horizon_y_pos = screen_height//2 + screen_height//8
-        self.obstacle_y_pos = screen_height//2 + screen_height//8 - o_height + 15
 
         self.screen_width = screen_width
         self.cloud_width = c_width
         self.cloud_height = c_height
-        self.obstacle_width = o_width
-        self.obstacle_height = o_height
 
         self.horizon_offset_x = 0
         self.cloud_offset_x = 0
-        self.obstacle_offset_x = 0
 
         self.cloud_asset = self._scale_asset_image(
             CLOUD, self.cloud_width, self.cloud_height)
         self.horizon_asset = self._scale_asset_image(
             HORIZON, self.screen_width, self.horizon_height)
-        self.obstacle_asset = self._scale_asset_image(
-            ONE_CACTUS, self.obstacle_width, self.obstacle_height)
 
         self.clouds = []
-        self.obstacles = []
-
         self._generate_clouds()
-        self._generate_obstacles()
 
     def _scale_asset_image(self, image, width, height):
         return pygame.transform.scale(image, (width, height)).convert_alpha()
@@ -58,22 +47,12 @@ class Environment:
             cloud_start = 0
         else:
             cloud_start = self.clouds[-1].x + random.randint(100, 300)
-            self.clouds = self.clouds[1:]  # Trim out the first cloud
 
         cloud_y = random.randint(15, 45)
+        self.clouds = self.clouds[1:]  # Trim out the first cloud
+
         self.clouds.extend([pygame.Rect(cloud_start + (i+1) * 200, cloud_y,
                            self.cloud_width, self.cloud_height) for i in range(random.randint(1, 3))])
-
-    def _generate_obstacles(self):
-        """Generate initial or additional obstacles"""
-        if len(self.obstacles) == 0:  # If no obstacles yet
-            obstacle_start = self.screen_width
-        else:
-            obstacle_start = self.obstacles[-1].x + self.screen_width + 100
-            self.obstacles.clear()
-
-        self.obstacles.extend([pygame.Rect(obstacle_start + i*50, self.obstacle_y_pos,
-                              self.obstacle_width, self.obstacle_height) for i in range(random.randint(1, 3))])
 
     def draw_horizon(self, screen):
         """Display horizon on screen"""
@@ -94,17 +73,7 @@ class Environment:
         if (self.clouds[0].x + self.cloud_offset_x + self.cloud_width) < 0:
             self._generate_clouds()
 
-    def draw_obstacles(self, screen):
-        """Display obstacles on screen"""
-        for obstacle in self.obstacles:
-            screen.blit(self.obstacle_asset, (obstacle.x +
-                        self.obstacle_offset_x, obstacle.y))
-
-        if (self.obstacles[-1].x + self.obstacle_offset_x + 100) < 0:
-            self._generate_obstacles()
-
     def animate(self):
         """Animate environment on screen"""
         self.horizon_offset_x -= self.horizon_velocity
-        self.obstacle_offset_x -= self.horizon_velocity
         self.cloud_offset_x -= self.cloud_velocity
--- main.py
+++ main.py
@@ -30,22 +30,31 @@ SCREEN_WIDTH, SCREEN_HEIGHT = 720, 400
 WINDOW = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 DINO_X_POS = 30
 DINO_Y_POS = SCREEN_HEIGHT//2 + SCREEN_HEIGHT//8 - DINO_HEIGHT + 15
+OBSTACLE_Y_POS = SCREEN_HEIGHT//2 + SCREEN_HEIGHT//8 - OBSTACLE_HEIGHT + 15
+OBSTACLE_DISTANCES = (50, 75, 100)
 
 pygame.display.set_caption("Gino")
 
+# Images
+ONE_CACTUS = pygame.transform.scale(pygame.image.load(os.path.join(
+    'Assets', 'sprites', '1_Cactus.png')), (OBSTACLE_WIDTH, OBSTACLE_HEIGHT)).convert_alpha()
+
+
 # Events
 SWITCH_FOOT = pygame.USEREVENT + 1
+ADD_OBSTACLES = pygame.USEREVENT + 2
 
 # Timers
 pygame.time.set_timer(SWITCH_FOOT, 125)
 
+obstacle_scroll_x = 0
 left_foot = True
 
 
-def draw_window(dino, environment, play=True):
+def draw_window(dino, environment, obstacles, play=True):
     """Handles repainting of screen surface
     """
-    # global obstacle_scroll_x
+    global obstacle_scroll_x
     global left_foot
 
     WINDOW.fill(WHITE)
@@ -54,12 +63,21 @@ def draw_window(dino, environment, play=True):
 
     environment.draw_horizon(WINDOW)
 
-    environment.draw_obstacles(WINDOW)
+    # Render obstacles on path
+    for obstacle in obstacles:
+        WINDOW.blit(ONE_CACTUS, (obstacle.x + obstacle_scroll_x, obstacle.y))
+
+    # Raise ADD_OBSTACLES event when the last of the obstacles goes off screen
+    if (obstacles[-1].x + obstacle_scroll_x + 100) < 0:
+        pygame.event.post(pygame.event.Event(ADD_OBSTACLES))
 
     # Normal game play if ON, static image if OFF
     if play:
         dino.move(left_foot)
         environment.animate()
+
+        obstacle_scroll_x -= HORIZON_VEL
+
     else:
         dino.stand()
 
@@ -78,7 +96,11 @@ def main():
 
     dino = Dino(DINO_X_POS, DINO_Y_POS, DINO_WIDTH, DINO_HEIGHT)
     environment = Environment(
-        SCREEN_WIDTH, SCREEN_HEIGHT, CLOUD_WIDTH, CLOUD_HEIGHT, OBSTACLE_WIDTH, OBSTACLE_HEIGHT)
+        SCREEN_WIDTH, SCREEN_HEIGHT, CLOUD_WIDTH, CLOUD_HEIGHT)
+
+    # Generate random number of obstacles to be placed on the road
+    obstacles = [pygame.Rect(SCREEN_WIDTH + 10 + i*50, OBSTACLE_Y_POS, OBSTACLE_WIDTH,
+                             OBSTACLE_HEIGHT) for i in range(random.randint(1, 3)) for i in range(random.randint(1, 3))]
 
     while game_running:
         # Poll for events
@@ -110,10 +132,23 @@ def main():
                 if event.type == SWITCH_FOOT:
                     left_foot = not left_foot  # Invert left_foot
 
-            # Draw window for subsequent screen.
-            draw_window(dino, environment)
+                # Handling the event to add obstacles to path
+                if event.type == ADD_OBSTACLES:
+                    obstacle_x = obstacles[-1].x + (SCREEN_WIDTH + 100)
 
-        draw_window(dino, environment, play)  # Draw initial screen.
+                    obstacles.clear()  # Clear previous obstacles
+
+                    obstacles.extend([pygame.Rect(obstacle_x + i*50, OBSTACLE_Y_POS,
+                                     OBSTACLE_WIDTH, OBSTACLE_HEIGHT) for i in range(random.randint(1, 3))])
+
+            # keys = pygame.key.get_pressed()
+            # if keys[pygame.K_SPACE]:
+            #     print("Jump")
+
+            # Draw window for subsequent screen.
+            draw_window(dino, environment, obstacles)
+
+        draw_window(dino, environment, obstacles, play)  # Draw initial screen.
 
     pygame.quit()
 
