9014d7d3cc1b97abedc8a80781015b7ff56e3cbb
PC-Ngumoha <pcngumoha@gmail.com>
2025-09-03 22:49:34 +0100

    Milestone: Add collision detection.

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ pygame.init()
 class Dino:
     """Dino class"""
 
-    def __init__(self, screen_height):
+    def __init__(self, screen_height: int):
         self.width = 80
         self.height = 80
         self.jump_pace = 25
-        self.fall_pace = 6
+        self.fall_pace = 5
         self.jumping = False
         self.falling = False
         self.left_foot = True
@@ -24,6 +24,7 @@ class Dino:
 
         self.x = 30
         self.y = screen_height//2 + screen_height//4 - self.height + self.height//4
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
 
         self.standing_sprite = pygame.transform.scale(pygame.image.load(os.path.join(
             'Assets', 'sprites', 'Dino_Standing.png')), (self.width, self.height))
@@ -32,7 +33,7 @@ class Dino:
         self.right_foot_sprite = pygame.transform.scale(pygame.image.load(os.path.join(
             'Assets', 'sprites', 'Dino_Right_Run.png')), (self.width, self.height))
 
-    def update(self, screen):
+    def update(self, screen: pygame.Surface) -> None:
         """Handles animating Dino on screen"""
         keys = pygame.key.get_pressed()
 
@@ -67,9 +68,16 @@ class Dino:
                 screen.blit(self.right_foot_sprite,
                             (self.x, self.y - self.offset_y))
 
-    def draw(self, screen):
+        self._update_rect()  # Update position of bounding box
+
+    def _update_rect(self) -> None:
+        """Update the position of the bounding box"""
+        self.rect.x = self.x
+        self.rect.y = self.y - self.offset_y
+
+    def draw(self, screen: pygame.Surface) -> None:
         """Draws Dino before game play starts."""
-        screen.blit(self.standing_sprite, (self.x, self.y))
+        screen.blit(self.standing_sprite, (self.x, self.y - self.offset_y))
 
     def switch_foot(self):
         """Trigger a change in sprite between left_foot and right foot"""
@@ -80,10 +88,10 @@ class Dino:
 class Environment:
     """Wrapper class for the game environment"""
 
-    def __init__(self, screen_width, screen_height):
+    def __init__(self, screen_width: int, screen_height: int):
         self.screen_width = screen_width
 
-        self.horizon_vel = 4.2
+        self.horizon_vel = 4.5
         self.horizon_tiles = 2
         self.horizon_offset_x = 0
 
@@ -101,13 +109,13 @@ class Environment:
 
         self._generate_cacti()
 
-    def draw_horizon(self, screen):
+    def draw_horizon(self, screen: pygame.Surface) -> None:
         """draw horizon"""
         for i in range(self.horizon_tiles):
             WINDOW.blit(self.horizon_sprite, (self.horizon_width * i +
                         self.horizon_offset_x, self.horizon_y))
 
-    def draw_cacti(self, screen):
+    def draw_cacti(self, screen: pygame.Surface) -> None:
         """draw cacti"""
         # Displaying the obstacles
         for cactus in self.cacti:
@@ -121,7 +129,15 @@ class Environment:
             # Generate a new set of obstacles:
             self._generate_cacti()
 
-    def update(self):
+    def detect_collision(self, dino_rect: pygame.Rect) -> None:
+        """Detect collision with dino"""
+        for cactus in self.cacti:
+            # Temporarily shift cactus rect
+            moved_rect = cactus.move(self.cactus_offset_x, 0)
+            if dino_rect.colliderect(moved_rect):
+                pygame.event.post(pygame.event.Event(COLLISION_DETECTED))
+
+    def update(self) -> None:
         """animate environment elements"""
         self.horizon_offset_x -= self.horizon_vel
         self.cactus_offset_x -= self.horizon_vel
@@ -129,7 +145,7 @@ class Environment:
         if abs(self.horizon_offset_x) > self.screen_width + 100:
             self.horizon_offset_x = 0
 
-    def _generate_cacti(self):
+    def _generate_cacti(self) -> None:
         """Utility function: generate new set of obstacles"""
         starting_point = 0
         if len(self.cacti) > 0:
@@ -140,7 +156,8 @@ class Environment:
         cactus_height = random.randint(50, 80)
         for i in range(num_cactus):
             cactus_y = self.horizon_y - cactus_height + cactus_height//4
-            cactus_x = starting_point + self.screen_width + i*self.cactus_width
+            cactus_x = starting_point + self.screen_width + \
+                i*(self.cactus_width - 10)
 
             self.cacti.append(pygame.Rect(
                 cactus_x, cactus_y, self.cactus_width, cactus_height))
@@ -160,11 +177,8 @@ pygame.display.set_caption("Gino")
 
 # User Events
 SWITCH_FOOT = pygame.USEREVENT + 1
+COLLISION_DETECTED = pygame.USEREVENT + 2
 
-
-# Global variables
-offset_x = 0
-obstacle_offset_x = 0
 
 # Timer to switch foot
 pygame.time.set_timer(SWITCH_FOOT, 125)
@@ -173,6 +187,8 @@ pygame.time.set_timer(SWITCH_FOOT, 125)
 def draw_window(play: bool, dino: Dino, environment: Environment) -> None:
 
     WINDOW.fill(WHITE)  # White
+
+    environment.detect_collision(dino.rect)
 
     environment.draw_horizon(screen=WINDOW)
     environment.draw_cacti(screen=WINDOW)
@@ -187,7 +203,7 @@ def draw_window(play: bool, dino: Dino, environment: Environment) -> None:
     pygame.display.update()
 
 
-def main():
+def main() -> None:
     """main code for the game.
     """
     game_running = True
@@ -219,10 +235,9 @@ def main():
                     # left_foot = not left_foot
                     dino.switch_foot()
 
-                # FIXME: experimental feature: testing pause functionality
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RETURN:
-                        play = False
+                # Fires when collision is detected, The game pauses
+                if event.type == COLLISION_DETECTED:
+                    play = False
 
             draw_window(play, dino=dino, environment=environment)
 
